bb89615cb4e3fe2342d99be79a05367623808035
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2023-12-18 01:03:13 +0300

    created folder for the pages

---
+++ Dart-Tasks/task-4/to_do_list_app/lib/pages/Firstscreen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:task/pages/Secondscreen.dart';
+class Firstscreen extends StatefulWidget {
+  @override
+  State<Firstscreen> createState() => _FirstscreenState();
+}
+
+class _FirstscreenState extends State<Firstscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: ListView(children: [
+      Image(
+        image: AssetImage(
+          'images/istockphoto-1271149787-612x612.jpg',
+        ),
+      ),
+      Padding(padding: EdgeInsets.symmetric(vertical: 70.0)),
+      Row(children: [
+        Padding(padding: EdgeInsets.symmetric(horizontal: 15.0)),
+        Container(
+          height: 40,
+          width: 250,
+          decoration: BoxDecoration(
+              color: Color.fromARGB(255, 71, 125, 206),
+              borderRadius: BorderRadius.circular(7)),
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Secondscreen()));
+            },
+            child: Text('Get Started',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center),
+          ),
+        )
+      ])
+    ]));
+  }
+}
